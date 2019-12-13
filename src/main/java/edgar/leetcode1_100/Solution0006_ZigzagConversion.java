e5b9eb73eeb9c4791af9aef7f011fb52aecc4ee2
EdgarLiu2 <42142693+EdgarLiu2@users.noreply.github.com>
2019-12-13 08:14:56 +0800

    7. 整数反转

--- src/main/java/edgar/leetcode1_100/Solution0006_ZigzagConversion.java
+++ src/main/java/edgar/leetcode1_100/Solution0006_ZigzagConversion.java
@@ -84,7 +84,7 @@ public class Solution0006_ZigzagConversion {
 		return result.toString();
 	}
 	
-	public static String convert(String s, int numRows) {
+	public static String convert2(String s, int numRows) {
 		if (numRows == 1) {
 			return s;
 		}
@@ -96,28 +96,17 @@ public class Solution0006_ZigzagConversion {
 		}
 		
 		int nextRow = 0;
-		boolean isDown = true;
+		boolean isDown = false;
 		for(int i = 0; i< length; i++) {
 			String t = String.valueOf(s.charAt(i));
 			arrays[nextRow].add(t);
 			
-			if (isDown) {
-				if (nextRow == numRows - 1) {
-					// 已经向下走到头，向上
-					nextRow--;
-					isDown = false;
-				} else {
-					nextRow++;
-				}
-			} else {
-				if (nextRow == 0) {
-					// 已经向上走到头，向下
-					nextRow++;
-					isDown = true;
-				} else {
-					nextRow--;
-				}
+			if (nextRow == 0 || nextRow == numRows - 1) {
+				// 当前行nextRow为0或numRows-1时，箭头发生反向转折
+				isDown = !isDown;
 			}
+			
+			nextRow += isDown ? 1 : -1;
 		}
 		
 		StringBuilder result = new StringBuilder();
@@ -127,6 +116,56 @@ public class Solution0006_ZigzagConversion {
 		
 		return result.toString();
 	}
+	
+	/**
+	 * 
+	 * 每一行字母的所有下标其实是有规则的
+	 * 我们先假定有 numRows=4 行来推导下，其中 2*numRows-2 = 6 , 我们可以假定为 step=2*numRows-2 ，我们先来推导下规则：
+	 * 
+	 * 第0行： 0 - 6 - 12 - 18
+	 * ==> 下标间距 6 - 6 - 6 ==> 下标间距 step - step - step
+	 * 第1行： 1 - 5 - 7 - 11 - 13
+	 * ==> 下标间距 4 - 2 - 4 - 2 ==> 下标间距step-2*1(行) - 2*1(行) - step-2*1(行) - 2*1(行)
+	 * 第2行： 2 - 4 - 8 - 10 - 14
+	 * ==> 下标间距 2 - 4 - 2 - 4 ==> 下标间距step-2*2(行) - 2*2(行) - step-2*2(行) - 2*2(行)
+	 * 第3行：3 - 9 - 15 - 21
+	 * ==> 下标间距间距 6 - 6 - 6 ==>下标间距step - step - step
+	 * 
+	 * 可以得出以下结论：
+	 * 1. 起始下标都是行号
+	 * 2. 第0层和第numRows-1层的下标间距总是step 。
+	 * 3. 中间层的下标间距总是step-2*行数，2*行数交替。
+	 * 4. 下标不能超过len(s)-1
+	 * 
+	 * @param s
+	 * @param numRows
+	 * @return
+	 */
+	public static String convert3(String s, int numRows) {
+		if (numRows == 1) {
+			return s;
+		}
+		
+		int step = numRows * 2 - 2;				// 间距
+		int index = 0;									// 记录s的下标
+		int len = s.length();
+		int add = 0;										// 真实的间距
+		StringBuilder result = new StringBuilder();
+		
+		for (int i = 0; i < numRows; i++) {
+			index = i;
+			add = i * 2;
+			while (index < len) {
+				// 超出字符串长度计算下一层
+				result.append(s.charAt(index));	// 当前行的第一个字母
+				add = step - add;						// 第一次间距是step-2*i，第二次是2*i, 
+				index += (i == 0 || i == numRows-1) ? step : add; // 0行和最后一行使用step间距，其余使用add间距
+			}
+		}
+		
+		return result.toString();
+	}
+	
 
 	/**
 	 * @param args
@@ -136,21 +175,21 @@ public class Solution0006_ZigzagConversion {
 		 * 
 		 * "LEETCODEISHIRING", 3, "LCIRETOESIIGEDHN"
 		 * "LEETCODEISHIRING", 4, "LDREOEIIECIHNTSG"
-		 * "PAHNAPLSIIGYIR", 3, "PAHNAPLSIIGYIR"
+		 * "PAHNAPLSIIGYIR", 3, "PAIIANPSIYRHLG"
 		 * "PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"
 		 * "A", 1
 		 * "AB", 1
 		 * 
 		 */
 
-		System.out.printf("input: %s, numRows: %d, result: %s\n", "LEETCODEISHIRING", 3, convert("LEETCODEISHIRING", 3));
-		System.out.printf("input: %s, numRows: %d, result: %s\n", "LEETCODEISHIRING", 4, convert("LEETCODEISHIRING", 4));
+		System.out.printf("input: %s, numRows: %d, result: %s\n", "LEETCODEISHIRING", 3, convert2("LEETCODEISHIRING", 3));
+		System.out.printf("input: %s, numRows: %d, result: %s\n", "LEETCODEISHIRING", 4, convert2("LEETCODEISHIRING", 4));
 		
-		System.out.printf("input: %s, numRows: %d, result: %s\n", "PAHNAPLSIIGYIR", 3, convert("PAHNAPLSIIGYIR", 3));
-		System.out.printf("input: %s, numRows: %d, result: %s\n", "PAYPALISHIRING", 3, convert("PAYPALISHIRING", 3));
+		System.out.printf("input: %s, numRows: %d, result: %s\n", "PAHNAPLSIIGYIR", 3, convert2("PAHNAPLSIIGYIR", 3));
+		System.out.printf("input: %s, numRows: %d, result: %s\n", "PAYPALISHIRING", 3, convert2("PAYPALISHIRING", 3));
 		
-		System.out.printf("input: %s, numRows: %d, result: %s\n", "A", 1, convert("A", 1));
-		System.out.printf("input: %s, numRows: %d, result: %s\n", "AB", 1, convert("AB", 1));
+		System.out.printf("input: %s, numRows: %d, result: %s\n", "A", 1, convert2("A", 1));
+		System.out.printf("input: %s, numRows: %d, result: %s\n", "AB", 1, convert2("AB", 1));
 	}
 
 }
---
+++ src/main/java/edgar/leetcode1_100/Solution0007_ReverseInteger.java
@@ -0,0 +1,62 @@
+package edgar.leetcode1_100;
+
+public class Solution0007_ReverseInteger {
+	
+	public static int reverse1(int x) {
+
+		boolean isPositive;
+		if (x > 0) {
+			isPositive = true;
+		} else {
+			isPositive = false;
+			x = -x;
+		}
+		int y = 0;
+		int MAX_Y_1 = (int)Math.pow(2, 31) / 10;
+		
+		while (x > 0) {
+			if (y > MAX_Y_1) {
+				return 0;
+			}
+			
+			int pop = x % 10;
+			x /= 10;
+			
+			y = y * 10 + pop;
+		}
+
+		return isPositive ? y : -y;
+	}
+	
+	public static int reverse(int x) {
+		
+		long y = 0;
+		while (x != 0) {
+			int pop = x % 10;
+			x /= 10;
+			
+			y = y * 10 + pop;
+			if (y > Integer.MAX_VALUE || y < Integer.MIN_VALUE) {
+				y = 0;
+				break;
+			}
+		}
+
+		return (int)y;
+	}
+
+	public static void main(String[] args) {
+		// 输入: 123，输出: 321
+		assert reverse(123) == 321;
+		
+		// 输入: -123，输出: -321
+		assert reverse(-123) == -321;
+		
+		// 输入: 120，输出: 21
+		assert reverse(120) == 21;
+		
+		// 输入: 1534236469，输出: 0
+		assert reverse(1534236469) == 0;
+	}
+
+}
