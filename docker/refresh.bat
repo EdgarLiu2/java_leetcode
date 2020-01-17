6ba7e1a4d64c4ee06f503164aaa9f6429feaa0ac
EdgarLiu2 <42142693+EdgarLiu2@users.noreply.github.com>
2020-01-17 09:13:36 +0800

    start zookeeper cluster by docker

--- docker/refresh.bat
+++ docker/refresh.bat
@@ -1,4 +1,4 @@
-
+REM COMPOSE_PROJECT_NAME=dev-leetcode
 docker-compose -p dev-leetcode -f docker-compose.dev.yml down
 docker-compose -p dev-leetcode -f docker-compose.dev.yml up -d
 
@@ -8,8 +8,35 @@ REM docker container exec -it dev-leetcode_redis-db_1 bash
 REM docker container exec -it dev-leetcode_canal-server_1 bash
 REM docker container exec -it dev-leetcode_rabbitmq_1 bash
 REM docker container exec -it dev-leetcode_etcd-1_1 bash
+REM docker container exec -it dev-leetcode_zookeeper-1_1 bash
 
 REM docker container logs dev-leetcode_mysql-db_1
 REM docker container logs dev-leetcode_canal-server_1
 REM docker container logs dev-leetcode_rabbitmq_1
 REM docker container logs dev-leetcode_etcd-1_1
+REM docker container logs dev-leetcode_zookeeper-1_1
+
+
+
+docker run -dit --name ubuntu ubuntu
+docker container exec -it ubuntu /bin/bash
+
+docker container exec -it dev-leetcode_ubuntu_1 bash
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+cat > /etc/apt/sources.list <<EOF
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+EOF
+apt-get update
+apt-get -y install vim
+apt-get -y install netcat-traditional	# nc
+apt-get -y install net-tools
+apt-get -y install iputils-ping
---
+++ src/main/java/edgar/zookeeper/QuickStart.md
@@ -0,0 +1,7 @@
+# Commands
+
+## Check Server Status
+./bin/zkServer.sh --config /conf  status
+echo stat | nc localhost 2181
+http://localhost:8080/commands
+./bin/zkCli.sh -server zookeeper-1:2181
