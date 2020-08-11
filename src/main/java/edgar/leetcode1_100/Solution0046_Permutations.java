6591aa8fe83c311bfbd2d188d2e2c1a4338aeb8f
liuzhao.edgar <liuzhao.edgar@bytedance.com>
2020-08-11 22:00:33 +0800

    46. 全排列

---
+++ src/main/java/edgar/leetcode1_100/Solution0046_Permutations.java
@@ -0,0 +1,117 @@
+package edgar.leetcode1_100;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 46. 全排列
+ * https://leetcode-cn.com/problems/permutations/
+ * 
+ * @author liuzhao
+ *
+ */
+public class Solution0046_Permutations {
+	
+	public static List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		if (nums.length == 0) {
+			return result;
+		} else if (nums.length == 1) {
+			List<Integer> r = Arrays.asList(nums[0]);
+			result.add(r);
+			return result;
+		}
+		// path用做一个栈
+		Deque<Integer> path = new ArrayDeque<>();
+		// 记录n个数字是否已经在使用
+		boolean[] used = new boolean[nums.length];
+		dfs(nums, nums.length, 0, path, used, result);
+		
+		return result;
+	}
+	
+	private static void dfs(int[] nums, int length, int depth, Deque<Integer> path, boolean[] used, List<List<Integer>> result) {
+		
+		if (length == depth) {
+			result.add(new ArrayList<>(path));
+			return;
+		}
+		
+		for (int i = 0; i < length; i++) {
+			if (used[i]) {
+				continue;
+			}
+			
+			path.addLast(nums[i]);
+			used[i] = true;
+			dfs(nums, length, depth + 1, path, used, result);
+			used[i] = false;
+			path.removeLast();
+		}
+	}
+
+	public static List<List<Integer>> permute2(int[] nums) {
+		
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		if (nums.length == 0) {
+			return result;
+		} else if (nums.length == 1) {
+			List<Integer> r = Arrays.asList(nums[0]);
+			result.add(r);
+			return result;
+		}
+		
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < nums.length; i++ ) {
+			set.add(nums[i]);
+		}
+		
+		dfs(set, new ArrayList<Integer>(), result);
+		
+		return result;
+	}
+	
+	public static void dfs(Set<Integer> unused_nums, List<Integer> tempResult, List<List<Integer>> result) {
+		if (unused_nums.isEmpty()) {
+			result.add(tempResult);
+			return;
+		}
+		
+		for (Integer currunt_num : unused_nums) {
+			Set<Integer> new_unused_nums = new HashSet<>(unused_nums);
+			new_unused_nums.remove(currunt_num);
+			
+			List<Integer> newTempResult = new ArrayList<>(tempResult);
+			newTempResult.add(currunt_num);
+			
+			dfs(new_unused_nums, newTempResult, result);
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] nums;
+		List<List<Integer>> result;
+		
+		/*
+		 * 输入: [1,2,3]
+		 * 输出:
+		 * [
+		 *   [1,2,3],
+		 *   [1,3,2],
+		 *   [2,1,3],
+		 *   [2,3,1],
+		 *   [3,1,2],
+		 *   [3,2,1]
+		 * ]
+		 */
+		nums= new int[] {1,2,3};
+		result = permute(nums);
+		System.out.println(result);
+	}
+
+}
