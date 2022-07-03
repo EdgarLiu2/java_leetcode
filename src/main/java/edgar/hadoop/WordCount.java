7338bc1eceb9b1879ca232a28625b5c55b638df6
liuzhao.edgar <liuzhao.edgar@bytedance.com>
2022-07-03 15:28:56 +0800

    尚硅谷大数据Hadoop3教程 - 4.3.1 InputFormat数据输入

--- src/main/java/edgar/hadoop/WordCount.java
+++ src/main/java/edgar/hadoop/WordCount.java
@@ -11,6 +11,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -67,7 +68,7 @@ public class WordCount {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    static void wordCount(String inputPath, String outputPath) throws Exception {
         // 1. 获取配置信息，获取Job对象
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "WordCount");
@@ -90,13 +91,52 @@ public class WordCount {
 
         // 6. 指定Job的输入文件目录
 //        FileInputFormat.addInputPath(job, new Path("/Users/liuzhao/Desktop/Bytedance/workspace/hadoop/hadoop_test/wordcount/input"));
-        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileInputFormat.addInputPath(job, new Path(inputPath));
 
         // 7. 指定Job的输出结果目录
 //        FileOutputFormat.setOutputPath(job, new Path("/Users/liuzhao/Desktop/Bytedance/workspace/hadoop/hadoop_test/wordcount/output"));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
 
         // 8. 提交Job
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    static void wordCountWithCombineTextInputFormat(String inputPath, String outputPath) throws Exception {
+        // 1. 获取配置信息，获取Job对象
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "WordCount");
+
+        // 2. 指定本程序的jar包所再的本地路径
+        job.setJarByClass(WordCount.class);
+
+        // 3. 关联Mapper/Reducer业务类
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+
+        // 4. 指定Mapper输出的<K, V>类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+
+        // 5. 指定最终输出的<K, V>类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        // 如果不设置，默认是TextInputFormat
+        job.setInputFormatClass(CombineTextInputFormat.class);
+        // 虚拟存储切片最大值：4MB
+        CombineTextInputFormat.setMaxInputSplitSize(job, 4 * 1024 * 1024);
+
+        // 6. 指定Job的输入文件目录
+        FileInputFormat.addInputPath(job, new Path(inputPath));
+
+        // 7. 指定Job的输出结果目录
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
+
+        // 8. 提交Job
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        wordCount(args[0], args[1]);
     }
 }
