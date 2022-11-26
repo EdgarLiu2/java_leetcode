8212622ff3fd545fe4931f82795b974b2cd7d105
liuzhao.edgar <liuzhao.edgar@bytedance.com>
2022-11-26 17:45:09 +0800

    尚硅谷Java11新特性教程

--- src/main/java/edgar/try_new/jdk11/NewOptional.java
+++ src/main/java/edgar/try_new/jdk11/NewOptional.java
@@ -11,6 +11,15 @@ public class NewOptional {
 		assert o.isEmpty();
 		// Java 8: 判断Optional内部是否有值
 		assert !o.isPresent();
+
+		// of()方法不能传入null
+		o = Optional.of(1);
+
+		// ofNullable()方法可以传入null
+		o = Optional.ofNullable(null);
+		assert o.isEmpty();
+
+		System.out.println(o.orElse(10));
 	}
 	
 	public static void main(String[] args) {
