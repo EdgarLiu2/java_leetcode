ae865aa77edf720d82989a397322e0608314f109
liuzhao.edgar <liuzhao.edgar@bytedance.com>
2023-02-08 00:13:47 +0800

    206. 反转链表

--- src/main/java/edgar/leetcode/Solution0206_ReverseLinkedList.java
+++ src/main/java/edgar/leetcode/Solution0206_ReverseLinkedList.java
@@ -1,20 +1,20 @@
 package edgar.leetcode;
 
 /**
- * Created by liuzhao on 2022/7/31
+ * <a href="https://leetcode.cn/problems/reverse-linked-list/">206. 反转链表</a>
+ * Created by Edgar.Liu on 2022/7/31
  */
 public class Solution0206_ReverseLinkedList {
-    public static ListNode reverseList(ListNode head) {
+
+    static ListNode reverseList1(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
 
         ListNode newHead = new ListNode(0);
 
-        // 当前的第一个节点
-        ListNode firstNode = head;
         // 将第一个节点后面的链表反转
-        ListNode restList = reverseList(firstNode.next);
+        ListNode restList = reverseList1(head.next);
         // 把后面链表放在前面
         newHead.next = restList;
         // 找到restList的尾部
@@ -22,9 +22,32 @@ public class Solution0206_ReverseLinkedList {
         while (tail.next != null) {
             tail = tail.next;
         }
-        // 将firstNode放在尾部
-        tail.next = firstNode;
-        firstNode.next = null;
+        // 将head放在尾部
+        tail.next = head;
+        head.next = null;
+
+        return newHead.next;
+    }
+
+    static ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // 新头节点，最后返回头节点的next
+        ListNode newHead = new ListNode(0);
+        ListNode p;
+
+        while (head != null) {
+            // p指向当前head链表的第一个
+            p = head;
+            // head向后移动
+            head = head.next;
+
+            // 将p插入到newHead链表的头部
+            p.next = newHead.next;
+            newHead.next = p;
+        }
 
         return newHead.next;
     }
@@ -34,27 +57,33 @@ public class Solution0206_ReverseLinkedList {
         int[] input;
         ListNode list;
 
-        /**
+        /*
          * 输入：head = [1,2,3,4,5]
          * 输出：[5,4,3,2,1]
          */
         input = new int[]{1, 2, 3, 4, 5};
         list = ListNode.buildList(input);
-        reverseList(list).print();
+        reverseList1(list).print();
+        list = ListNode.buildList(input);
+        reverseList2(list).print();
 
-        /**
+        /*
          * 输入：head = [1,2]
          * 输出：[2,1]
          */
         input = new int[]{1, 2};
         list = ListNode.buildList(input);
-        reverseList(list).print();
+        reverseList1(list).print();
+        list = ListNode.buildList(input);
+        reverseList2(list).print();
 
-        /**
+        /*
          * 输入：head = []
          * 输出：[]
          */
         input = new int[]{};
+        list = ListNode.buildList(input);
+        assert list == null;
         list = ListNode.buildList(input);
         assert list == null;
     }
