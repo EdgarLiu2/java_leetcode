a082b9dbbd87cbf7a247e7fbf0664c43245e3c6a
liuzhao.edgar <liuzhao.edgar@bytedance.com>
2023-04-13 08:36:27 +0800

    69. x 的平方根

--- src/main/java/edgar/leetcode/Solution0069_Sqrtx.java
+++ src/main/java/edgar/leetcode/Solution0069_Sqrtx.java
@@ -1,13 +1,12 @@
 package edgar.leetcode;
 
-/*
- * 69. x 的平方根
- * https://leetcode-cn.com/problems/sqrtx/
- * 
+/**
+ * <a href="https://leetcode-cn.com/problems/sqrtx/">69. x 的平方根</a>
+ * Created by Edgar.Liu on 2023/4/13
  */
 public class Solution0069_Sqrtx {
 
-    public static int mySqrt(int x) {
+	static int mySqrt(int x) {
     	
     	long left = 1;
     	long right = x;
@@ -32,6 +31,30 @@ public class Solution0069_Sqrtx {
 
     	return result;
     }
+
+	static int mySqrt2(int x) {
+
+		if (x <= 1) {
+			return x;
+		}
+
+		long result = -1;
+		long left = 1;
+		long right = x;
+
+		while (left <= right) {
+			long mid = left + (right - left) / 2;
+
+			if (mid * mid <= x) {
+				result = mid;
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+
+		return (int)result;
+	}
     
 	public static void main(String[] args) {
 		/*
@@ -39,24 +62,28 @@ public class Solution0069_Sqrtx {
 		 * 输出: 46339
 		 */
 		assert 46339 == mySqrt(2147395599);
-		
+		assert 46339 == mySqrt2(2147395599);
+
 		/*
 		 * 输入: 8
 		 * 输出: 2
 		 */
 		assert 2 == mySqrt(8);
-		
+		assert 2 == mySqrt2(8);
+
 		/*
 		 * 输入: 1
 		 * 输出: 1
 		 */
 		assert 1 == mySqrt(1);
-		
+		assert 1 == mySqrt2(1);
+
 		/*
 		 * 输入: 4
 		 * 输出: 2
 		 */
 		assert 2 == mySqrt(4);
+		assert 2 == mySqrt2(4);
 
 	}
 
