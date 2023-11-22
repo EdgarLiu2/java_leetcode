21441376dcce8a6a78784d69b35141d2d8a6e68f
liuzhao.edgar <liuzhao.edgar@bytedance.com>
2023-11-22 18:18:18 +0800

    LCR 194. 二叉树的最近公共祖先

--- src/main/java/edgar/leetcode/Solution0236_LowestCommonAncestorOfABinaryTree.java
+++ src/main/java/edgar/leetcode/Solution0236_LowestCommonAncestorOfABinaryTree.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 /**
  * <a href="https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/">236. 二叉树的最近公共祖先</a>
+ * <a href="https://leetcode.cn/problems/er-cha-shu-de-zui-jin-gong-gong-zu-xian-lcof/">LCR 194. 二叉树的最近公共祖先</a>
  *
  * @author Edgar.Liu
  * @since 2023/11/21 - 07:48
@@ -51,6 +52,47 @@ public class Solution0236_LowestCommonAncestorOfABinaryTree {
         fillParentMap(node.right, node, parentMap);
     }
 
+    static TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        return doLowestCommonAncestor2(root, p, q).answer;
+    }
+
+    static LowestCommonAncestorInfo doLowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            // 空树一定没有找到，直接返回信息
+            return new LowestCommonAncestorInfo(null, false, false);
+        }
+
+        // 从左右子树获取信息
+        LowestCommonAncestorInfo leftInfo = doLowestCommonAncestor2(root.left, p, q);
+        LowestCommonAncestorInfo rightInfo = doLowestCommonAncestor2(root.right, p, q);
+
+        // 开始汇总当前节点的信息
+
+        // 当前root节点就是p，或者左右子树已经找到p，表示当前子树已经找到p
+        boolean isFindP = root == p || leftInfo.isFindP || rightInfo.isFindP;
+        // 当前root节点就是q，或者左右子树已经找到q，表示当前子树已经找到q
+        boolean isFindQ = root == q || leftInfo.isFindQ || rightInfo.isFindQ;
+
+        // 当前子树的最后答案
+        TreeNode answer = null;
+        if (leftInfo.answer != null) {
+            // 左子树已经找到答案，那就是当前子树的最后答案
+            answer = leftInfo.answer;
+        }
+        if (rightInfo.answer != null) {
+            // 右子树已经找到答案，那就是当前子树的最后答案
+            answer = rightInfo.answer;
+        }
+
+        if (answer == null && isFindP && isFindQ) {
+            // 如果当前子树已经找到p和q，那么相交节点就是当前的root节点
+            answer = root;
+        }
+
+        return new LowestCommonAncestorInfo(answer, isFindP, isFindQ);
+    }
+
+
     public static void main(String[] args) {
         TreeNode root, p, q, ans;
 
@@ -72,6 +114,8 @@ public class Solution0236_LowestCommonAncestorOfABinaryTree {
                 q);
         ans = lowestCommonAncestor1(root, p, q);
         assert ans != null && ans.val == 3;
+        ans = lowestCommonAncestor2(root, p, q);
+        assert ans != null && ans.val == 3;
 
         /*
          * 输入：root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
@@ -91,6 +135,8 @@ public class Solution0236_LowestCommonAncestorOfABinaryTree {
                         new TreeNode(8)));
         ans = lowestCommonAncestor1(root, p, q);
         assert ans != null && ans.val == 5;
+        ans = lowestCommonAncestor2(root, p, q);
+        assert ans != null && ans.val == 5;
 
         /*
          * 输入：root = [1,2], p = 1, q = 2
@@ -103,5 +149,19 @@ public class Solution0236_LowestCommonAncestorOfABinaryTree {
         root = p;
         ans = lowestCommonAncestor1(root, p, q);
         assert ans != null && ans.val == 1;
+        ans = lowestCommonAncestor2(root, p, q);
+        assert ans != null && ans.val == 1;
+    }
+}
+
+class LowestCommonAncestorInfo {
+    public TreeNode answer;
+    public boolean isFindP;
+    public boolean isFindQ;
+
+    public LowestCommonAncestorInfo(TreeNode answer, boolean isFindP, boolean isFindQ) {
+        this.answer = answer;
+        this.isFindP = isFindP;
+        this.isFindQ = isFindQ;
     }
 }
